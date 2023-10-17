4bd1c3f95b7462454afca0e207a61e0f692df745
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-17 14:16:48 +0500

    LockAnimation-SwiftUI

---
+++ LockAnimation/LockView.swift
@@ -0,0 +1,20 @@
+//
+//  LockView.swift
+//  LockAnimation
+//
+//  Created by Zaid Sheikh on 17/10/2023.
+//
+
+import SwiftUI
+
+struct LockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LockView_Previews: PreviewProvider {
+    static var previews: some View {
+        LockView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# LockAnimation-SwiftUI
