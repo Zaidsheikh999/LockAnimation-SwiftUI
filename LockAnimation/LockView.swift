11a71db7ef40e3bbc0fad212db42c9b687ec2195
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-17 14:17:08 +0500

    LockAnimation-SwiftUI

--- LockAnimation/LockView.swift
+++ LockAnimation/LockView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct LockView: View {
+    
+    @State var isLock: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+        
+            ZStack{
+                Circle()
+                    .trim( from: 0, to: 0.5)
+                    .stroke(isLock ? Color.green : Color.gray, style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                    .frame(width: 160, height: 160, alignment: .center)
+                    .rotationEffect(.degrees(isLock ? 270 : 180))
+                    .animation(Animation.easeInOut(duration: 0.5).repeatCount(1))
+                    .onAppear() {
+                        self.isLock.toggle()
+                    }.offset(x: 0, y: -133)
+                
+            }
+            
+            RoundedRectangle(cornerRadius: 25)
+                .foregroundColor(.black)
+                .foregroundColor(isLock ? .green : .white)
+                .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            
+            VStack {
+                Text( isLock  ? "Unlock" : "Lock")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    .foregroundColor(isLock ? .green : .white)
+                Toggle("", isOn: $isLock)
+                    .labelsHidden()
+            }.padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 25)
+                        .stroke(lineWidth: 5)
+                        .foregroundColor(isLock ? .green : .white)
+                        .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                )
+        }
     }
 }
 
-struct LockView_Previews: PreviewProvider {
-    static var previews: some View {
-        LockView()
-    }
-}
