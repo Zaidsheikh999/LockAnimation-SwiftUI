5de12db38677c123250700990d376d143cd61ba7
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-17 14:10:42 +0500

    Initial Commit

---
+++ LockAnimation/LockAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockAnimationApp.swift
+//  LockAnimation
+//
+//  Created by Zaid Sheikh on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LockAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
